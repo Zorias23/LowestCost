4a6d93f8296690665ac631c86afb8edab53a04c1
Zorias23 <rhuxley23@gmail.com>
2018-02-01 08:42:23 -0700

    The classes MatrixTester and MatrixTesterRunner have been deleted.  The testing class in the unit test module has been renamed to MatrixUnitTest

--- app/src/test/java/com/example/admin/lowestcost/MatrixUnitTest.java
+++ app/src/test/java/com/example/admin/lowestcost/MatrixUnitTest.java
@@ -1,72 +1,23 @@
 package com.example.admin.lowestcost;
 
-import junit.framework.TestCase;
+import org.junit.Test;
+import com.example.admin.lowestcost.Graph.RoutingTable;
 
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
-import com.example.admin.lowestcost.Graph.RoutingTable;
+
+import static org.junit.Assert.*;
 
 /**
- * Created by Admin on 1/30/2018.
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
  */
+public class MatrixUnitTest {
 
-/**
- * To be run with MatrixTesterRunner class
- */
-public class MatrixTester extends TestCase{
-    /*This class has had its code commented out because the logic has been moved to the unit test module
-    /*
-    private String methodName;
-    public MatrixTester(String s)
-    {
-        methodName = s;
-    }
-    /**
-     * If the matrix has been loaded properly and with proper values. the underlying RoutingTable should have both available paths and visited
-     * paths
-     * @param filename
-     * @throws FileNotFoundException
 
-    public void test5DimenMatrix(String filename) {
-        Graph.RoutingTable r = null;
-        try {
-            r = createMatrixPaths(filename, 5);
-        } catch (FileNotFoundException e) {
-            fail("File does not exist.  Matrix not created.");
-        }
-        assertTrue(r.getAvailablePaths() != null && r.getAvailablePaths().size() > 0);
-        assertTrue(r.getVisitedPaths() != null && r.getVisitedPaths().size() > 0);
-    }
-
-    public void test3DimenMatrix(String filename)
-    {
-        Graph.RoutingTable r = null;
-        try {
-            r = createMatrixPaths(filename, 3);
-        } catch (FileNotFoundException e) {
-            fail("File does not exist.  Matrix not created.");
-        }
-        assertTrue(r.getAvailablePaths() != null && r.getAvailablePaths().size() > 0);
-        assertTrue(r.getVisitedPaths() != null && r.getVisitedPaths().size() > 0);
-    }
-
-    public void testNDimenMatrix(String filename, int n)
-    {
-        Graph.RoutingTable r = null;
-        if (n > 100)
-        {
-            //this was part of the design specs
-            fail("More than 100 dimensions are not allowed for a Matrix.");
-        }
-        try {
-            r = createMatrixPaths(filename, n);
-        } catch (FileNotFoundException e) {
-            fail("File does not exist.  Matrix not created.");
-        }
-        assertTrue(r.getAvailablePaths() != null && r.getAvailablePaths().size() > 0);
-        assertTrue(r.getVisitedPaths() != null && r.getVisitedPaths().size() > 0);
-    }
+    @Test
     public void testInputFile(String fileName)
     {
         try {
@@ -87,12 +38,55 @@ public class MatrixTester extends TestCase{
                     }
                 }
             }
-         //we should be able to read the file and fetch each token of data as an integer, if not, we have an invalid file
+            //we should be able to read the file and fetch each token of data as an integer, if not, we have an invalid file
         } catch (Exception e)
         {
             fail("Exception caught while trying to parse file.  Invalid input file.");
         }
     }
+    @Test
+    public void test5DimenMatrix(String filename) {
+        RoutingTable r = null;
+        try {
+            r = createMatrixPaths(filename, 5);
+        } catch (FileNotFoundException e) {
+            fail("File does not exist.  Matrix not created.");
+        }
+        assertTrue(r.getAvailablePaths() != null && r.getAvailablePaths().size() > 0);
+        assertTrue(r.getVisitedPaths() != null && r.getVisitedPaths().size() > 0);
+    }
+
+    @Test
+    public void test3DimenMatrix(String filename)
+    {
+        Graph.RoutingTable r = null;
+        try {
+            r = createMatrixPaths(filename, 3);
+        } catch (FileNotFoundException e) {
+            fail("File does not exist.  Matrix not created.");
+        }
+        assertTrue(r.getAvailablePaths() != null && r.getAvailablePaths().size() > 0);
+        assertTrue(r.getVisitedPaths() != null && r.getVisitedPaths().size() > 0);
+    }
+
+    @Test
+    public void testNDimenMatrix(String filename, int n)
+    {
+        Graph.RoutingTable r = null;
+        if (n > 100)
+        {
+            //this was part of the design specs
+            fail("More than 100 dimensions are not allowed for a Matrix.");
+        }
+        try {
+            r = createMatrixPaths(filename, n);
+        } catch (FileNotFoundException e) {
+            fail("File does not exist.  Matrix not created.");
+        }
+        assertTrue(r.getAvailablePaths() != null && r.getAvailablePaths().size() > 0);
+        assertTrue(r.getVisitedPaths() != null && r.getVisitedPaths().size() > 0);
+    }
+
     public Graph.RoutingTable createMatrixPaths(String fileName, int nodes) throws FileNotFoundException
     {
         Graph dijkstras = new Graph(7, fileName);
@@ -109,5 +103,6 @@ public class MatrixTester extends TestCase{
         }
         return Route1;
     }
-    */
+
+
 }
